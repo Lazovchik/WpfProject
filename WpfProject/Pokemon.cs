60512476b4eaac0169f946fff767f1ae83b13544
Lazovchik <33687555+Lazovchik@users.noreply.github.com>
2020-03-29 14:35:57 +0200

    Pokemon class implemented

--- WpfProject/Pokemon.cs
+++ WpfProject/Pokemon.cs
@@ -1,7 +1,58 @@
-﻿namespace WpfProject
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfProject
 {
-    public class POkemon
+    public class Pokemon: INotifyPropertyChanged
     {
+        private string url;
+        private int id;
+        private string name;
+        private string imgUrl;
         
+        public string Url
+        {
+            get { return url; }
+            set
+            {
+                url = value;
+                OnPropertyChanged("Url");
+            }
+        }
+        public int ID
+        {
+            get { return id; }
+            set
+            {
+                id= value;
+                OnPropertyChanged("ID");
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string ImgUrl
+        {
+            get { return imgUrl; }
+            set
+            {
+                ImgUrl = value;
+                OnPropertyChanged("ImgUrl");
+            }
+        }
+
+        //implementation of INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
